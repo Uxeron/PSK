8a0dc1052d3ea4df4f549ae20d3303e617edf85b
Nerijus Pūras <nerijus.puras@devbridge.com>
2022-04-10 19:34:06 +0300

    PSK-40: initial User endpoints

---
+++ WebAPI/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿namespace WebAPI.Services;
+
+using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class UserService : IUserService
+{
+    private readonly PskContext _context;
+
+    public UserService(PskContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> GetUser(Guid id)
+    {
+        return await _context.Users.Where(i => i.UserId == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task CreateUser(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+}
