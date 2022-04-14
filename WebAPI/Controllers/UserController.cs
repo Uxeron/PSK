2fb17b00bc61c85796584324dcf56cc890c532ee
Nerijus Pūras <nerijus.puras@devbridge.com>
2022-04-14 15:34:46 +0300

    PSK-40: Pr fix meaningful return

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -31,6 +31,6 @@ public class UserController : ControllerBase
     public async Task<IActionResult> CreateUser([FromBody] User user)
     {
         await _userService.CreateUser(user);
-        return NoContent();
+        return Ok();
     }
 }
