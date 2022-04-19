bbf59b5fb3622837f8835c94d3fb3b5afbdfe075
VeronikaSum <veronika.sumilova@gmail.com>
2022-04-19 22:03:09 +0300

    Created one to many relationship between Item ang Images

---
+++ Data/Migrations/20220419183941_CreateImageTable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class CreateImageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Items");
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ImageId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ImageData = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ItemId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_Image_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_ItemId",
+                table: "Image",
+                column: "ItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Items",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- Data/Models/Item/Item.cs
+++ Data/Models/Item/Item.cs
@@ -5,7 +5,6 @@ public class Item
     public Guid ItemId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string Image { get; set; } = string.Empty;
     public ItemCondition Condition { get; set; }
     public ItemCategory Category { get; set; }
     public bool IsGivenAway { get; set; }
@@ -15,4 +14,8 @@ public class Item
     public DateTime UpdateDate { get; set; }
     public User? User { get; set; }
     public Address? Address { get; set; }
+
+    public ICollection<Image> Images { get; set; } = new List<Image>();
+
+
 }
