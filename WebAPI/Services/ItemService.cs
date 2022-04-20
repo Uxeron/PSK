4a159e879a52a828cb901990f1a9ddae8a04ec0d
VeronikaSum <veronika.sumilova@gmail.com>
2022-04-20 17:08:41 +0300

    Added image saving method, conversion to byte array and rhe other way around

--- WebAPI/Services/ItemService.cs
+++ WebAPI/Services/ItemService.cs
@@ -56,6 +56,40 @@ public class ItemService : IItemService
         await _context.SaveChangesAsync();
     }
 
+    private async void saveImages(String imageName, String imageData) //Image name - uploaded item name
+    {
+        string[] imagesData = splitBase64String(imageData);
+
+        foreach (var data in imagesData)
+        {
+            Image imageEntity = new Image();
+            byte[] imageBytes = convertBase64ToBytes(data);
+
+            imageEntity.Name = imageName;
+            imageEntity.ImageData = imageBytes;
+
+            await _context.Images.AddAsync(imageEntity);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+
+    private string[] splitBase64String(String imageData)
+    {
+        return String.Join("", imageData.Split(','))
+            .Split(new string[] { "data:image/jpeg;base64", "data:image/png;base64" }, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    private byte[] convertBase64ToBytes(String encodedImage)
+    {
+        return Convert.FromBase64String(encodedImage);
+    }
+
+    private String convertBytesToBase64(byte[] imageBytes)
+    {
+        return Convert.ToBase64String(imageBytes);
+    }
+
     public async Task<Item?> GetItem(Guid id)
     {
         return await _context.Items.Where(i => i.ItemId == id).FirstOrDefaultAsync();
