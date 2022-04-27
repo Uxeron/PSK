c240062d6735f4f972281fea05d5dd0fc8f800f9
VeronikaSum <veronika.sumilova@gmail.com>
2022-04-27 18:47:47 +0300

    formatting change

--- Data/Models/Image.cs
+++ Data/Models/Image.cs
@@ -6,8 +6,6 @@ public class Image
     public byte[] ImageData { get; set; } = new byte[0];
     public byte[] ThumbnailImageData { get; set; } = new byte[0];
     public string Name { get; set; } = string.Empty;
-
     public string Prefix { get; set; } = string.Empty;
-
     public Item Item { get; set; } = new Item();
 }
