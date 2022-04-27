adcdba110cb103bbe0387de322a287a95f4a1387
Uxeron <vgabrielius123@gmail.com>
2022-04-27 22:37:16 +0300

    Removed Item reference from Image class, was causing cyclic issues

---
+++ Data/Migrations/20220427192554_RemovedItemFromImage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class RemovedItemFromImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Items_ItemId",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ItemId",
+                table: "Images",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Items_ItemId",
+                table: "Images",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "ItemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Items_ItemId",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ItemId",
+                table: "Images",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Items_ItemId",
+                table: "Images",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "ItemId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
