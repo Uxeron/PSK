555ddc39d7648e30c11aeb9a627a4146e0029fb9
VeronikaSum <veronika.sumilova@gmail.com>
2022-04-27 20:26:38 +0300

    Fixes after PR comments

--- WebAPI/Services/ItemService.cs
+++ WebAPI/Services/ItemService.cs
@@ -50,17 +50,17 @@ public class ItemService : IItemService
         return itemEntity;
     }
 
-    private async Task<List<Data.Models.Image>> SaveImages(String imageName, String imageData) //Image name - uploaded item name
+    private async Task<List<Data.Models.Image>> SaveImages(string imageName, string imageData) //Image name - uploaded item name
     {
         string[] imagesData = SplitBase64String(imageData);
-        List<Data.Models.Image> images = new List<Data.Models.Image>();
+        var images = new List<Data.Models.Image>();
 
         for (int i = 0; i < imagesData.Length; i++)
         {
             Data.Models.Image imageEntity = new Data.Models.Image();
             if (imagesData[i].StartsWith("data:image/"))
             {
-                byte[] imageBytes = ConvertBase64ToBytes(imagesData[i + 1]);
+                byte[] imageBytes = Convert.FromBase64String(imagesData[i + 1]);
                 byte[] resizedImage = ResizeImage(imageBytes);
 
                 imageEntity.Name = imageName;
@@ -80,7 +80,7 @@ public class ItemService : IItemService
 
     }
 
-    private byte[] ResizeImage(byte[] byteImageIn)
+    private static byte[] ResizeImage(byte[] byteImageIn)
     {
         Bitmap startBitmap;
         using (var ms = new MemoryStream(byteImageIn))
@@ -101,16 +101,6 @@ public class ItemService : IItemService
     private string[] SplitBase64String(String imageData)
     {
         return imageData.Split(',');
-    }
-
-    private byte[] ConvertBase64ToBytes(String encodedImage)
-    {
-        return Convert.FromBase64String(encodedImage);
-    }
-
-    private String ConvertBytesToBase64(byte[] imageBytes)
-    {
-        return Convert.ToBase64String(imageBytes);
     }
 
     public async Task<Item?> GetItem(Guid id)
@@ -140,7 +130,7 @@ public class ItemService : IItemService
                 ItemId = i.ItemId,
                 Name = i.Name,
                 Description = i.Description,
-                Image = ConvertBytesToBase64(i.Images.First().ThumbnailImageData),
+                Image = Convert.ToBase64String(i.Images.First().ThumbnailImageData),
                 Condition = i.Condition,
                 Category = i.Category,
                 UploadDate = i.UploadDate,
