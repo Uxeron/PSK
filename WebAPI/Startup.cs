e599c92bf1ca3f591943cdc7cd47c58e5dc90cc1
Uxeron <vgabrielius123@gmail.com>
2022-05-10 16:32:46 +0300

    Moved startup commands to Startup class, added config reading

---
+++ WebAPI/Startup.cs
@@ -0,0 +1,75 @@
+﻿namespace WebAPI;
+
+using Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using WebAPI.Services;
+using WebAPI.Services.Interfaces;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        }).AddJwtBearer(options =>
+        {
+            options.Authority = Configuration.GetSection("Auth0")["Authority"];
+            options.Audience = Configuration.GetSection("Auth0")["Audience"];
+            options.RequireHttpsMetadata = false;
+        });
+
+        services.AddCors();
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddDbContext<PskContext>();
+
+        services.AddScoped<IItemService, ItemService>();
+        services.AddScoped<IAddressService, AddressService>();
+        services.AddScoped<IUserService, UserService>();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        // Configure the HTTP request pipeline.
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseDeveloperExceptionPage();
+
+        app.UseAuthentication();
+
+        using (var scope = app.ApplicationServices.CreateScope())
+        using (var context = scope.ServiceProvider.GetRequiredService<PskContext>())
+        context.Database.EnsureCreated();
+
+        app.UseHttpsRedirection();
+
+        app.UseCors(builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
